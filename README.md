7b625f4d41232b0622b038499018b77fcf9a0b63
Rajarshi Guha <rajarshi_guha@vrtx.com>
2019-12-17 18:03:51 -0500

    Added code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rgroup-pie
 PoC code to generate a structure depictions with partial pie charts centered on R group atoms
+
+The code is inspired by Fig 3 in [Doyon et al](https://pubs.acs.org/doi/10.1021/jacs.9b10474)
+which was developed by [Attabey Rodríguez Benítez](https://twitter.com/ScienceBey). The original
+visualization was designed to summarize substrate scope but can be generalized to plot any number(s)
+associated with R-groups (e.g., bioactivity)
+
+The code uses the [OEChem](https://www.eyesopen.com/oechem-tk) so you'll need a license to run it.
+
+There is an example MOL file with a single R group - I've hardcoded the program to look for `R1`. It 
+doesn't color the segments by gradient but that is relatively straightforward
---
+++ rgrouppie.py
@@ -0,0 +1,83 @@
+import math
+
+from openeye import oechem, oedepict, oegrapheme
+from openeye.oechem import OEBlack, OEGraphMol, OEReadMolecule, OEWhite
+from openeye.oedepict import OE2DMolDisplay
+
+# Constants
+RGRP_IDX = 1
+WEDGE_WIDTH = 30
+RADIUS_BASE = 40
+START_ANGLE = 5
+SEGMENT_LABEL_SCALE_FACTOR = 1.3
+
+ifs = oechem.oemolistream()
+ifs.open("rgrouppie.mol")
+mol = OEGraphMol()
+OEReadMolecule(ifs, mol)
+
+######################################
+
+oedepict.OEPrepareDepiction(mol)
+mdisp = OE2DMolDisplay(mol)
+
+# Get display coordinates of the R group atom
+r_coords = None
+for atom in mol.GetAtoms():
+    if atom.GetMapIdx() == 1:
+        r_coords = mdisp.GetAtomDisplay(atom).GetCoords()
+
+# Create base image
+image = oedepict.OEImage(300, 300)
+
+# Draw pie segments
+vals = [[100, ">99", 87, 12],
+        [78, 12, 34, 77]]
+n_ring = len(vals)
+
+# Get the layer into which we will draw the pie chart
+layer = mdisp.GetLayer(oedepict.OELayerPosition_Below)
+
+# Set up pens
+pen_green = oedepict.OEPen(oechem.OELightGreen, oechem.OEBlack, oedepict.OEFill_On, 1.0)
+pen_red = oedepict.OEPen(oechem.OELightPurple, oechem.OEBlack, oedepict.OEFill_On, 1.0)
+pens = [pen_green, pen_red]
+
+# Draw rings from outside inwards
+for ring_num in range(n_ring):
+    starting_angle = START_ANGLE
+    ring = vals[ring_num]
+    for value in ring:
+        radius = (n_ring - ring_num) * RADIUS_BASE
+        layer.DrawPie(r_coords, starting_angle, starting_angle + WEDGE_WIDTH, radius, pens[ring_num])
+
+        # Write value in segment - based on
+        # https://docs.eyesopen.com/toolkits/cookbook/python/_downloads/properties2img.py
+        p = oedepict.OE2DPoint(0, -radius * 0.75)
+        midangle = (starting_angle + starting_angle + WEDGE_WIDTH) / 2.0
+        rad = math.radians(midangle)
+        cosrad = math.cos(rad)
+        sinrad = math.sin(rad)
+        txt_coord = r_coords + oedepict.OE2DPoint(cosrad * p.GetX() - sinrad * p.GetY(),
+                                                  sinrad * p.GetX() + cosrad * p.GetY())
+
+        fontsize = int(SEGMENT_LABEL_SCALE_FACTOR * math.sqrt(radius))
+        font = oedepict.OEFont(oedepict.OEFontFamily_Default, oedepict.OEFontStyle_Bold, fontsize,
+                               oedepict.OEAlignment_Center, oechem.OEBlack)
+        label = oedepict.OEHighlightLabel(str(value), font)
+        label.SetBoundingBoxPen(oedepict.OETransparentPen)
+        oedepict.OEAddLabel(layer, txt_coord, label)
+
+        starting_angle += WEDGE_WIDTH
+
+# Draw circle round R atom on top of pie chart center
+pen = oedepict.OEPen(OEWhite, OEBlack, oedepict.OEFill_On, 1)
+glyph = oegrapheme.OEAtomGlyphCircle(pen, oegrapheme.OECircleStyle_Default, 1.5)
+oegrapheme.OEAddGlyph(mdisp, glyph, oechem.OEHasMapIdx(1))
+
+# Render molecule
+oedepict.OERenderMolecule(image, mdisp)
+
+# Write out images
+oedepict.OEWriteImage("MolWithWedgeChart.svg", image)
+oedepict.OEWriteImage("MolWithWedgeChart.png", image)
